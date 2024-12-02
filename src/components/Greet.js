623ec3eb9380bb2d1141f0e1571d5c56adf13b8d
Syeda Bisma Jafri <zahoorkurazme@gmail.com>
2024-12-02 23:24:57 +0500

    Add files via upload

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import react from "react"
+
+// function Greet () { 
+    // return <h1>I am Bismah Jaffery </h1> 
+// } 
+
+// export default Greet 
+
+// ARROW FUNction
+
+export const  Greet = () => <h1>Hello, I am Syeda Bismah Jaffery.</h1> //export default bhi karsakty hein laikin aisay bh export krwa sakty hein.
